1004631cd5d2c6751b9a85b69c4b8ff61913ed8d
Edwok <fisheroliver9@gmail.com>
2020-10-27 18:47:34 +0300

    feat: initial implementation data protection for mongo db

---
+++ src/MongoDataProtectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Microsoft.AspNetCore.DataProtection.Mongo
+{
+    public static class MongoDataProtectionExtensions
+    {
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified key in Mongo database
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="databaseFactory">The delegate used to create <see cref="IMongoDatabase"/> instances.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToMongo(this IDataProtectionBuilder builder, Func<IMongoDatabase> databaseFactory)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (databaseFactory == null)
+            {
+                throw new ArgumentNullException(nameof(databaseFactory));
+            }
+
+            builder.Services.Configure<KeyManagementOptions>(options =>
+            {
+                options.XmlRepository = new MongoXmlRepository(databaseFactory);
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ src/MongoXmlRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using MongoDB.Driver;
+
+namespace Microsoft.AspNetCore.DataProtection.Mongo
+{
+    /// <summary>
+    /// An XML repository backed by a Mongo list entry.
+    /// </summary>
+    public class MongoXmlRepository : IXmlRepository
+    {
+        private const string DataProtectionCollectionName = "DataProtectionKeys";
+
+        private readonly Func<IMongoDatabase> _databaseFactory;
+
+        /// <summary>
+        /// Creates a <see cref="MongoXmlRepository"/> with keys stored at the given directory.
+        /// </summary>
+        /// <param name="databaseFactory">The delegate used to create <see cref="IMongoDatabase"/> instances.</param>
+        public MongoXmlRepository(Func<IMongoDatabase> databaseFactory)
+        {
+            _databaseFactory = databaseFactory;
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            var database = _databaseFactory();
+            var collection = database.GetCollection<string>(DataProtectionCollectionName);
+
+            return GetAllElementsCore().ToList().AsReadOnly();
+
+            IEnumerable<XElement> GetAllElementsCore()
+            {
+                var elements = collection
+                    .Find(FilterDefinition<string>.Empty)
+                    .ToCursor();
+
+                foreach (var value in elements.ToEnumerable())
+                {
+                    yield return XElement.Parse(value);
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            var database = _databaseFactory();
+            var collection = database.GetCollection<string>(DataProtectionCollectionName);
+
+            collection.InsertOne(element.ToString(SaveOptions.DisableFormatting));
+        }
+    }
+}
